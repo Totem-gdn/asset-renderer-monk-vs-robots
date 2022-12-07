e1ff94102c5aa5995bf9d020aaad7ceceda25ea2
illiadimension <ilya.nikulin@it-dimension.com>
2022-12-07 10:25:22 +0200

    return item render

--- controllers/api/nft.js
+++ controllers/api/nft.js
@@ -2,8 +2,6 @@
 const path = require('path');
 const nftHelper = require('../../helpers/dna-parser')
 const gm = require('gm');
-console.log('qweqwe');
-
 
 
 class NFTController {
@@ -23,59 +21,22 @@ class NFTController {
       // 1 - Skinny // 0 - Fat
       // 1 - Wimp // 0 - buff
 
-      console.log('time', new Date().getTime());
       
-      // const render = type === 'avatar' ? await mergeAvatar(nft) : await mergeItem(nft);
+      const render = type === 'avatar' ? await mergeAvatar(nft) : await mergeItem(nft);
       const percentForScale = 100 / 1080 * (width > height ? width : height);
 
-      const folderPath = path.resolve(`resources/avatar/`)
-  const sex = nft.sex_bio === 0 ? 'Man' : 'Woman';
-  const type = nft.body_type === 0 ? 'Fat' : 'Skinny';
-  const str = nft.body_strength === 0 ? '_buff' : '';
-      gm()
-        .in('-fill', nft.human_eye_color)
-        .in('-opaque', '#00ff00')
-        .in(`${folderPath}/${sex}/${type}${str}/Mask/Eye_Mask.png`)
+      gm(render)
+        .transparent('white')
+        // .in('-resize', `${width}x${height}`)
+        .in('-scale', `${percentForScale}%`)
+        .in(type === 'avatar' ? '-flop' : '')
 
-        .in('-fill', nft.human_skin_color)
-        .in('-opaque', '#00ff00')
-        .in(`${folderPath}/${sex}/${type}${str}/Mask/Skin_Mask.png`)
-
-        .in('-fill', nft.primary_color)
-        .in('-opaque', '#00ff00')
-        .in(`${folderPath}/${sex}/${type}${str}/Mask/Suit_Mask.png`)
-
-        .in('-fill', nft.human_hair_color)
-        .in('-opaque', '#00ff00')
-        .in(`${folderPath}/${sex}/${type}${str}/Mask/Hair/${nft.hair_styles}.png`)
-
-        .in('-compose', 'Multiply')
-        .in(`${folderPath}/${sex}/${type}${str}/${nft.hair_styles}.png`)
-        .background('#2a2d33')
-        .flatten()
-        .stream('png').pipe(res)
-        // .toBuffer((err, buff) => {
-        //   gm(buff)
-        //     .transparent('white')
-        //     // .in('-resize', `${width}x${height}`)
-        //     .in('-scale', `${percentForScale}%`)
-        //     .in(type === 'avatar' ? '-flop' : '')
-        //     .in('-bordercolor', 'none')
-        //     .in('-border', '10x100')
-        //     .stream('png').pipe(res)
-        // })
-    
-       
-      console.log('time2', new Date().getTime());
-
+        .stream('png')
+        .pipe(res)
 
     } else {
       res.status(404).json({ error: 'File not found' })
     }
-// 1 ресайз вихідних зображень
-// падінги 
-// зробити глобальні обєекти з картинок shared object
-// маски - 
 
   }
 
@@ -86,9 +47,9 @@ function mergeAvatar(nft) {
   const sex = nft.sex_bio === 0 ? 'Man' : 'Woman';
   const type = nft.body_type === 0 ? 'Fat' : 'Skinny';
   const str = nft.body_strength === 0 ? '_buff' : '';
-  // return new Promise((resolve, reject) => {
-    // try {
-    return  gm()
+  return new Promise((resolve, reject) => {
+    try {
+      gm()
         .in('-fill', nft.human_eye_color)
         .in('-opaque', '#00ff00')
         .in(`${folderPath}/${sex}/${type}${str}/Mask/Eye_Mask.png`)
@@ -112,10 +73,10 @@ function mergeAvatar(nft) {
         .toBuffer((err, buff) => {
           resolve(buff)
         })
-  //   } catch (error) {
-  //     reject(error);
-  //   }
-  // })
+    } catch (error) {
+      reject(error);
+    }
+  })
 }
 
 function mergeItem(nft) {
